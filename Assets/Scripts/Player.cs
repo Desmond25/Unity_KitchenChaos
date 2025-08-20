4cf6b30164cd828d9e17024b7a19724325a15b69
Arkady Shenteryakov <83672488+Desmond25@users.noreply.github.com>
2025-08-20 17:12:28 +0400

    Interact Action, C# Events

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,32 @@ public class Player : MonoBehaviour
     private bool _isWalking = false;
     private Vector3 _lastInteractDir;
 
+    private void Start()
+    {
+        _gameInput.OnInteractAction += _gameInput_OnInteractAction;
+    }
+
+    private void _gameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            _lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, _lastInteractDir, out RaycastHit raycastHit, interactDistance, _countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent<ClearCounter>(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+    }
+
     private void Update()
     {
         HandleMovement();
@@ -99,7 +125,7 @@ public class Player : MonoBehaviour
         {
             if (raycastHit.transform.TryGetComponent<ClearCounter>(out ClearCounter clearCounter))
             {
-                clearCounter.Interact();
+                //clearCounter.Interact();
             }
         }
     }
